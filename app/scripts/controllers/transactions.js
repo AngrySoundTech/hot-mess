89878a7029a2e4ddecd82ffab8071ac56183f780
Nicholas <nickcfeldman@gmail.com>
2017-03-27 00:59:55 -0400

    Update money on next digest cycle

--- app/scripts/controllers/transactions.js
+++ app/scripts/controllers/transactions.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('hotMessApp')
-  .controller('TransCtrl', ["$scope","$rootScope", "$location", "currentAuth", function ($scope, $rootScope, $location, currentAuth) {
+  .controller('TransCtrl', ["$scope","$rootScope", "$timeout", "$location", "currentAuth", function ($scope, $rootScope, $timeout, $location, currentAuth) {
     $scope.user = currentAuth;
 
     var moneyRef = firebase.database().ref('users/' + currentAuth.uid + '/money');
     moneyRef.on('value', function(money) {
-      $scope.$apply(function(){
-        $rootScope.money = money.val();
-      });
+        $timeout(function() {
+          $rootScope.money = money.val();
+        });
 
     });
 
