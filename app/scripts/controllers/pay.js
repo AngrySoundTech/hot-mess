8b4c4755b91fdbeeef9cc4ec9e28e101e729318e
Nicholas <nickcfeldman@gmail.com>
2017-03-27 01:26:49 -0400

    Add transfering money to other account

--- app/scripts/controllers/pay.js
+++ app/scripts/controllers/pay.js
@@ -1,11 +1,13 @@
 'use strict';
 
 angular.module('hotMessApp')
-  .controller('PayCtrl', ["$scope", "$location", "currentAuth", function ($scope, $location, currentAuth) {
+  .controller('PayCtrl', ["$scope", "$rootScope", "$location", "currentAuth", function ($scope, $rootScope, $location, currentAuth) {
     $scope.user = currentAuth;
 
 
     let usersRef = firebase.database().ref('users/');
+    let currUserRef = firebase.database().ref('users/' + $scope.user.uid);
+
     usersRef.once('value').then(function (snapshot) {
       $scope.$apply(function(){
         $scope.users = snapshot.val();
@@ -14,7 +16,23 @@ angular.module('hotMessApp')
 
     $scope.paySomeone = function () {
       if ($scope.uid && $scope.amount) {
-        console.log("Paid " + $scope.uid + " " + $scope.amount); // TODO: Actually Pay
+
+        if ($scope.amount > $rootScope.money) {
+          console.log("You don't have enough") //TODO Tell the user they're dumb
+        } else {
+          // Subtract money from current user
+          currUserRef.update({
+            money: $rootScope.money - $scope.amount
+          });
+          // Add money to new user
+          let targetUserRef = firebase.database().ref('users/' + $scope.uid);
+          targetUserRef.once('value').then(function (snapshot) {
+            let targetUserAmount = snapshot.val().money;
+            targetUserRef.update({
+              money: targetUserAmount + $scope.amount
+            })
+          });
+        }
       }
     }
 
