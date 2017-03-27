76565f242b40eece186edfc62a50bd2fa4756549
Nicholas <nickcfeldman@gmail.com>
2017-03-27 00:50:57 -0400

    Populate send money form

--- app/scripts/controllers/pay.js
+++ app/scripts/controllers/pay.js
@@ -4,8 +4,18 @@ angular.module('hotMessApp')
   .controller('PayCtrl', ["$scope", "$location", "currentAuth", function ($scope, $location, currentAuth) {
     $scope.user = currentAuth;
 
-    $scope.paySomeone = function (person, amount) {
 
+    let usersRef = firebase.database().ref('users/');
+    usersRef.once('value').then(function (snapshot) {
+      $scope.$apply(function(){
+        $scope.users = snapshot.val();
+      });
+    });
+
+    $scope.paySomeone = function () {
+      if ($scope.uid && $scope.amount) {
+        console.log("Paid " + $scope.uid + " " + $scope.amount); // TODO: Actually Pay
+      }
     }
 
   }]);
