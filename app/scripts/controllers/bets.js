6cbda510345973187f777b60fc2ac916040627c1
Nicholas <nickcfeldman@gmail.com>
2017-03-28 14:08:06 -0400

    Ability to create bets

--- app/scripts/controllers/bets.js
+++ app/scripts/controllers/bets.js
@@ -2,4 +2,26 @@ angular.module('hotMessApp')
   .controller('BetsCtrl', ["$scope","$rootScope", "$timeout", "$location", "currentAuth", function ($scope, $rootScope, $timeout, $location, currentAuth) {
     $scope.user = currentAuth;
 
+    let activeBetsRef = firebase.database().ref("bets/active");
+    let inactiveBetsRef = firebase.database().ref("bets/inactive");
+
+    activeBetsRef.on('value', (activeBets) => {
+      $timeout(() => {
+        let data = activeBets.val();
+        $scope.activeBets = Object.keys(data).map((key) => {
+          let obj = data[key];
+          obj._key = key;
+          return obj;
+        });
+      });
+    });
+
+    $scope.createBet = (description) => {
+      activeBetsRef.push({
+        description: description,
+        creator: $scope.user.uid,
+        creatorDisplayName: $scope.user.displayName,
+      });
+      $location.path('/bets');
+    };
   }]);
