981e5c3b984208821a055e08524add0fa6a073ac
Nicholas <nickcfeldman@gmail.com>
2017-03-30 19:14:01 -0400

    Finish Admin view

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Ripped straight from the pay controller. God have mercy on my soul
 angular.module('hotMessApp').controller('AdminCtrl', ["$scope", "auth", "currentAuth", "$location", "$timeout", function ($scope, auth, currentAuth, $location, $timeout) {
   $scope.user = currentAuth;
 
@@ -14,4 +15,35 @@ angular.module('hotMessApp').controller('AdminCtrl', ["$scope", "auth", "current
       });
     }
   });
+
+  let usersRef = firebase.database().ref('users/');
+  usersRef.once('value').then(function (snapshot) {
+    $scope.$apply(function(){
+      $scope.users = objectToList(snapshot.val());
+    });
+  });
+
+  $scope.paySomeone = function () {
+    if ($scope.selectedPerson && $scope.amount >= 1) {
+
+      // TODO: This should be a function on the server so people can't abuse it.
+      // Add money to new user
+      let targetUserRef = firebase.database().ref('users/' + $scope.selectedPerson.uid);
+      targetUserRef.once('value').then(function (snapshot) {
+        let targetUserName = snapshot.val().displayName;
+        let targetUserAmount = snapshot.val().money;
+        targetUserRef.update({
+          money: targetUserAmount + Math.floor($scope.amount)
+        });
+      });
+      $location.path('/')
+    }
+  };
+
+  $scope.searchUsers = (searchText) => {
+    return $scope.users.filter((value) => {
+      return value.displayName.toLowerCase().startsWith(searchText.toLowerCase());
+    });
+  }
+
   }]);
