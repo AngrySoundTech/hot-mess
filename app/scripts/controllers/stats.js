43c1233385d8f2397f83d622cfdc8a55650db32e
Nicholas <nickcfeldman@gmail.com>
2017-03-31 09:25:12 -0400

    Provide users and total Money for stats page

--- app/scripts/controllers/stats.js
+++ app/scripts/controllers/stats.js
@@ -3,4 +3,20 @@
 angular.module('hotMessApp').controller('StatsCtrl', ["$scope", "auth", "currentAuth", "$location", "$timeout", function ($scope, auth, currentAuth, $location, $timeout) {
   $scope.user = currentAuth;
 
+  $scope.totalMoney = 0;
+  $scope.users = [];
+  firebase.database().ref('/users/').on('value', users => {
+    //users = users.val();
+    let total = 0;
+    users.forEach(user => {
+      total += user.val().money;
+      $scope.users.push(user.val());
+    });
+    $timeout(() => {
+      $scope.totalMoney = total;
+      $scope.users.sort((a,b) => {
+        return b.money - a.money;
+      });
+    });
+  });
 }]);
